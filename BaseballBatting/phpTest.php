f77fd19a67882aeaa2f3ab2fc4789a7f4a8174d7
Josh Ludwig <joshua.r.ludwig@vanderbilt.edu>
2021-11-05 14:00:25 -0500

    Pushed MySQL file and a couple front-end files

---
+++ BaseballBatting/phpTest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Baseball Batting</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<div id="navbar">
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="phpTest.php">Test</a></li>
+    </ul>
+</div>
+
+<h1> Baseball Batting Database </h1>
+</body>
+</html>
---
+++ BaseballBatting/project2.sql
@@ -0,0 +1,53 @@
+-- Josh Ludwig, Lloyd Abernathy
+-- joshua.r.ludwig@vanderbilt.edu, 
+-- Project 2
+
+DROP DATABASE IF EXISTS baseballdb;
+CREATE DATABASE IF NOT EXISTS baseballdb;
+USE baseballdb;
+
+DROP TABLE IF EXISTS megatable;
+CREATE TABLE IF NOT EXISTS megatable (
+	player_ID VARCHAR(20),
+    player_name TINYTEXT, # need to fix player names that contain special characters
+    game_date VARCHAR(20), # some dates are not in date format, therefore VARCHAR instead of DATE
+    player_team CHAR(3),
+    opp_team CHAR(3),
+    game_result VARCHAR(10),
+	plate_app TINYINT UNSIGNED,
+    at_bat TINYINT UNSIGNED,
+    runs TINYINT UNSIGNED,
+    hits TINYINT UNSIGNED,
+    doubles TINYINT UNSIGNED,
+    triples TINYINT UNSIGNED,
+    homeruns TINYINT UNSIGNED,
+    RBIs TINYINT UNSIGNED,
+    base_on_balls TINYINT UNSIGNED,
+    int_base_on_balls TINYINT UNSIGNED,
+    strikeouts TINYINT UNSIGNED,
+    hit_by_pitch TINYINT UNSIGNED,
+    sacrifice_hits TINYINT UNSIGNED,
+    sacrifice_flies TINYINT UNSIGNED,
+    reached_on_error TINYINT UNSIGNED,
+    grounded_into_double_play TINYINT UNSIGNED,
+    stolen_bases TINYINT UNSIGNED,
+    caught_stealing TINYINT UNSIGNED,
+    win_prob_added DECIMAL(4,3),
+    base_out_runs_added DECIMAL(4,3),
+    average_leverage_index DECIMAL(4,3),
+    batting_order_position TINYINT UNSIGNED,
+    position VARCHAR(3),
+    draftkings_fantasy DECIMAL(5,2),
+    fanduel_fantasy DECIMAL(5,2)
+) ENGINE = INNODB;
+
+LOAD DATA LOCAL INFILE 'C:/Users/ludwj/Downloads/mlbbatting1901-2021.csv'  INTO TABLE megatable 
+CHARACTER SET latin1 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SELECT *
+FROM megatable
+ORDER BY game_date DESC;
+    
+    
