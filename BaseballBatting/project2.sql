8887196fb4a0fc34ff8d69d76752d16b76f50679
Josh Ludwig <joshua.r.ludwig@vanderbilt.edu>
2021-12-05 00:07:56 -0600

    Database updates

--- BaseballBatting/project2.sql
+++ BaseballBatting/project2.sql
@@ -90,6 +90,109 @@ GROUP BY player_ID, player_name, game_date
 HAVING count(*) > 1;
 
 
+-- Stored Procedure to get Career Stats based on player_id
+DELIMITER //
+DROP PROCEDURE IF EXISTS search_career //
+CREATE PROCEDURE search_career(IN ID VARCHAR(20))
+BEGIN
+	DECLARE player_name_ TINYTEXT;
+    DECLARE first_game_date VARCHAR(20);
+    DECLARE last_game_date VARCHAR(20);
+    DECLARE total_plate_app BIGINT UNSIGNED;
+    DECLARE total_at_bat BIGINT UNSIGNED;
+    DECLARE total_runs BIGINT UNSIGNED;
+    DECLARE total_hits BIGINT UNSIGNED;
+    DECLARE total_doubles BIGINT UNSIGNED;
+    DECLARE total_triples BIGINT UNSIGNED;
+    DECLARE total_homeruns BIGINT UNSIGNED;
+    DECLARE total_RBIS BIGINT UNSIGNED;
+    DECLARE total_strikeouts BIGINT UNSIGNED;
+    DECLARE total_walks BIGINT UNSIGNED;
+    DECLARE batting_average DECIMAL(4,3);
+    
+    SELECT player_name INTO player_name_ FROM player WHERE player_ID = ID LIMIT 1;
+    SELECT game_date INTO first_game_date FROM common_player_stats_by_game WHERE player_ID = ID ORDER BY game_date LIMIT 1;
+    SELECT game_date INTO last_game_date FROM common_player_stats_by_game WHERE player_ID = ID ORDER BY game_date DESC LIMIT 1;
+    
+    SELECT SUM(plate_app), SUM(at_bat), SUM(runs), SUM(hits), SUM(doubles), SUM(triples), SUM(homeruns), SUM(RBIS), SUM(strikeouts), SUM(base_on_balls)
+    INTO total_plate_app, total_at_bat, total_runs, total_hits, total_doubles, total_triples, total_homeruns, total_RBIS, total_strikeouts, total_walks
+    FROM common_player_stats_by_game WHERE player_ID = ID;
+    
+    SET batting_average = total_hits / total_at_bat;
+    
+    SELECT player_name_, first_game_date, last_game_date, total_plate_app, total_at_bat, 
+    total_runs, total_hits, total_doubles, total_triples, total_homeruns, total_RBIS, total_strikeouts, total_walks, batting_average;
+END //
+DELIMITER ;
+CALL search_career('altuvjo01');
+
+
+CREATE OR REPLACE VIEW atbatsLB AS
+SELECT player_name, SUM(plate_app) AS total_at_bat
+FROM common_player_stats_by_game 
+	JOIN player USING (player_ID)
+GROUP BY player_ID
+ORDER BY total_at_bat DESC;
+
+CREATE OR REPLACE VIEW hitsLB AS
+SELECT player_name, SUM(hits) AS total_hit
+FROM common_player_stats_by_game
+	JOIN player USING (player_ID)
+GROUP BY player_ID
+ORDER BY total_hit DESC;
+
+CREATE OR REPLACE VIEW homerunsLB AS
+SELECT player_name, SUM(homeruns) AS total_homeruns
+FROM common_player_stats_by_game
+	JOIN player USING (player_ID)
+GROUP BY player_ID
+ORDER BY total_homeruns DESC;
+
+CREATE OR REPLACE VIEW RBIsLB AS
+SELECT player_name, SUM(RBIs) AS total_RBIs
+FROM common_player_stats_by_game
+	JOIN player USING (player_ID)
+GROUP BY player_ID
+ORDER BY total_RBIs DESC;
+
+SELECT * FROM atbatsLB;
+SELECT * FROM hitsLB;
+SELECT * FROM homerunsLB;
+SELECT * FROM RBIsLB;
+
+
+DROP TRIGGER IF EXISTS remove_from_common_table;
+DELIMITER //
+CREATE TRIGGER remove_from_common_table
+AFTER DELETE
+ON player
+FOR EACH ROW
+BEGIN
+	DELETE FROM common_player_stats_by_game
+    WHERE player_ID = OLD.player_ID;
+END //
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS remove_from_uncommon_table;
+DELIMITER //
+CREATE TRIGGER remove_from_uncommon_table
+AFTER DELETE
+ON player
+FOR EACH ROW
+BEGIN
+	DELETE FROM uncommon_player_stats_by_game
+    WHERE player_ID = OLD.player_ID;
+END //
+DELIMITER ;
+
+-- DELETE FROM player
+-- WHERE player_ID = "rosepe01";
+
+select * from common_player_stats_by_game where player_ID = "rosepe01";
 
 select count(*) from megatable;
+select count(*) from common_player_stats_by_game;
+select count(*) from uncommon_player_stats_by_game;
+select count(*) from player;
 
+select * from megatable order by game_date desc;
